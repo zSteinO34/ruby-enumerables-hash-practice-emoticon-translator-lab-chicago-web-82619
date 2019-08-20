52c55b8828b70c28827c3f065858799bc800aa2a
Zac Steinberg <zstein1234@gmail.com>
2019-08-20 20:53:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,25 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  emoticons.each { |key, value|
+    hash["get_meaning"][value[1]] = key
+    hash["get_emoticon"][value[0]] = value[1]
+  }
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  hash = load_library(file_path)
+  hash.each { |key, value|
+    
+  }
+  binding.pry
 end
 
 def get_english_meaning
